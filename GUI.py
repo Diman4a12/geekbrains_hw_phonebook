707026359f79f020cedc4f8ff088ef2a26d47cf2
Diman4a12 <Diman4a65@gmail.com>
2024-03-26 19:08:19 +0500

    table ok

--- GUI.py
+++ GUI.py
@@ -2,14 +2,14 @@ from tkinter import *
 from tkinter import ttk
 
 root = Tk()
-root.title('Телефонный справочник')
-root.geometry('500x500')
+root.title("Телефонный справочник")
+root.geometry("800x400")
 
 
 def create_entry():
     add_phone = Tk()
-    add_phone.title('Добавить абонента')
-    add_phone.geometry('400x400')
+    add_phone.title("Добавить абонента")
+    add_phone.geometry("400x400")
     frame = ttk.Frame(add_phone, padding=[8, 10])
     frame.grid()
     ttk.Label(frame, text="Введите имя").pack(anchor=NW)
@@ -30,17 +30,18 @@ def create_entry():
 
 def change_entry():
     change_phone = Tk()
-    change_phone.title('Изменить запись')
-    change_phone.geometry('400x400')
+    change_phone.title("Изменить запись")
+    change_phone.geometry("400x400")
     frame = ttk.Frame(change_phone, padding=[8, 10])
     frame.grid()
     ttk.Button(frame, text="Сохранить").pack(anchor=NW)
     return frame
 
+
 def del_entry():
     del_phone = Tk()
-    del_phone.title('Удалить запись')
-    del_phone.geometry('210x80')
+    del_phone.title("Удалить запись")
+    del_phone.geometry("210x80")
     frame = ttk.Frame(del_phone, padding=[8, 10])
     frame.grid()
     ttk.Label(frame, text="Вы действительно хотите удалить?").pack(anchor=CENTER)
@@ -48,21 +49,26 @@ def del_entry():
     return frame
 
 
-
-
-
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-columns = ('name', 'surname', 'phone 1', 'phone 2', 'date birth', 'email')
-tree = ttk.Treeview(frm, columns=columns, show='headings').grid(column=1, row=0)
-#tree.heading('name', text='Имя')
-#tree.heading('surname', text='Имя')
-#tree.heading('phone 1', text='Имя')
-#tree.heading('phone 2', text='Имя')
-#tree.heading('date birth', text='Имя')
-#tree.heading('email', text='Имя')
-ttk.Button(frm, text="Добавить", command=create_entry).grid(column=1, row=10)
-ttk.Button(frm, text="Выбрать").grid(column=2, row=10)
-ttk.Button(frm, text="Изменить", command=change_entry).grid(column=3, row=10)
-ttk.Button(frm, text="Удалить", command=del_entry).grid(column=4, row=10)
-ttk.Button(frm, text="Выход", command=root.destroy).grid(column=5, row=10)
+columns = ("name", "surname", "phone 1", "phone 2", "date birth", "email")
+tree = ttk.Treeview(frm, columns=columns, show="headings")
+tree.grid(columnspan=5, row=0)
+root.grid_columnconfigure(0, weight=1)
+tree.heading("name", text="Имя")
+tree.heading("surname", text="Фамилия")
+tree.heading("phone 1", text="Телефон 1")
+tree.heading("phone 2", text="Телефон 2")
+tree.heading("date birth", text="Дата рождения")
+tree.heading("email", text="E-mail")
+tree.column("#1", stretch=NO, width=130)
+tree.column("#2", stretch=NO, width=130)
+tree.column("#3", stretch=NO, width=130)
+tree.column("#4", stretch=NO, width=130)
+tree.column("#5", stretch=NO, width=130)
+tree.column("#6", stretch=NO, width=130)
+ttk.Button(frm, text="Добавить", command=create_entry).grid(column=0, row=10)
+ttk.Button(frm, text="Выбрать").grid(column=1, row=10)
+ttk.Button(frm, text="Изменить", command=change_entry).grid(column=2, row=10)
+ttk.Button(frm, text="Удалить", command=del_entry).grid(column=3, row=10)
+ttk.Button(frm, text="Выход", command=root.destroy).grid(column=4, row=10)
